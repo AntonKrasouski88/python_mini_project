b8f559b5de0ab59d0e4f37cc43063030270276de
Anton Krasousky <antohkad21@gmail.com>
2023-09-03 12:14:01 +0300

    Add: game magic ball 8

---
+++ magic_ball_8/magic_ball_8.py
@@ -0,0 +1,43 @@
+# Game: magic ball 8
+import random  # Connect the module random
+
+answers = ['Бесспорно', 'Предрешено', 'Никаких сомнений', 'Определённо да', 'Можешь быть уверен в этом',
+           'Мне кажется - да', 'Вероятнее всего', 'Хорошие перспективы', 'Знаки говорят - да', 'Да',
+           'Пока неясно, попробуй снова', 'Спроси позже', 'Лучше не рассказывать', 'Сейчас нельзя предсказать',
+           'Сконцентрируйся и спроси опять', 'Даже не думай', 'Мой ответ - нет', 'По моим данным - нет',
+           'Перспективы не очень хорошие', 'Весьма сомнительно']
+
+
+def choice(arr):
+    num = random.randint(1, 20)
+    input('Задай вопрос на который ты хочешь получить ответ: ')
+    return arr[num]
+
+
+def check_answer():
+    ans = input('Хочешь задать еще один вопросы [ДА: enter "Y", НЕТ: enter "N"]: ')
+    while True:
+        if ans.lower() == 'y':
+            return True
+        elif ans.lower() == 'n':
+            return False
+        else:
+            print('Ошибка, тебе нужно ввести [ДА: enter "Y" или НЕТ: enter "N"]')
+            ans = input()
+
+
+def get_answer(arr):
+    print('Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.')
+    name = input('Как тебя зовут: ')
+    print(f'Привет {name}')
+
+    while True:
+        print(choice(arr))
+        flag = check_answer()
+        if not flag:
+            break
+        
+    print('Возвращайся если возникнут вопросы!')
+
+
+get_answer(answers)
