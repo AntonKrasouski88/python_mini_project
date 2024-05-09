1e544eb026edbd0383577eb82f8d01e3665a8709
Anton Krasouvski <antohkad21@gmail.com>
2024-05-09 10:00:28 +0300

    Exercise from the book: The total price

---
+++ tasks/tasks.py
@@ -0,0 +1,23 @@
+# 4. The total sales
+# Налог с продаж
+TAX = 0.07 
+
+# Создаем список с ценами на продукты
+products = []
+
+# Стоимость товаров
+cost_goods = 0
+
+# Принимаем стоимоть пяти товаров
+for i in range(5):
+    price = int(input('Enter the cost of the goods: '))
+    products.append(price)
+
+for el in products:
+    cost_goods += el
+
+
+tax_goods = cost_goods * TAX
+tax_cost = cost_goods + tax_goods
+
+print(f'Стоисть товаров: {cost_goods:,.2f}$', f'Налог состовляет: {tax_goods:,.2f}$', f'Итого стоимость к оплате: {tax_cost:,.2f}', sep = '\n')
